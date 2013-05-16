c88bed2746f2b1cce9b2e1f0d53fa21e12f62a0c
vansue <trongnghiahp85@yahoo.com>
2013-05-16 16:23:08 +0700

    pds2html
    Created on 15/05/2013

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset='UTF-8' />
+	<title>Start up</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css" />
+	<!--<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>-->
+	<script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>
+	<script type="text/javascript" src="js/example.js"></script>
+</head>
+
+<body>
+</body>
+
+</html>
